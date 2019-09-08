e3f0c0af4e696b5f8dcfa21cd79fe03c7ef0ce06
user <user@LAPTOP-LQN9H88F>
2019-09-08 10:12:25 -0500

    Third commit

--- Test/src/Test.java
+++ Test/src/Test.java
@@ -3,5 +3,6 @@ public class Test {
 	public static void main(String[] args) {
 		System.out.println("I wonder if this will work.");
 		System.out.println("It did.");
+		System.out.println("Fuck.");
 	}
 }
